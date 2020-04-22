c416c86d8ba04d6fded96953491fd0ffd289c89f
matt2256 <mads@mmm-m.dk>
2020-04-22 10:02:41 +0200

    Lavede hele interfacet om, forde jeg ikke kunne finde ud af at lave det på den måde som jeg gerne ville med data binding

--- WpfApp/WpfApp/MainWindow.xaml.cs
+++ WpfApp/WpfApp/MainWindow.xaml.cs
@@ -28,75 +28,57 @@ namespace WpfApp
         #endregion
 
 
-        #region Lister
-        List<Vare> vare = new List<Vare>() { };
-        List<Vare> AntalVareTilbage = new List<Vare> () { };
-        List<Vare> VareOversigt = new List<Vare>() 
-        {
-        };
-        #endregion
+        
+       
 
         public MainWindow()
         {
             InitializeComponent();
 
-            TilføjIs();
-            Pung();
+            
             DataContext = this;
         }
 
-        #region Tilføjelse af is data til listbox type 
-        //Tilføjer data til listboxen
-        public void TilføjIs()
+        private void BtnMagnum_Click(object sender, RoutedEventArgs e)
         {
-            VareOversigt.Add(new Vare("Magnum", 12));
-            VareOversigt.Add(new Vare("Filur", 13));
-            VareOversigt.Add(new Vare("Twister", 42));
-            VareOversigt.Add(new Vare("Københavnerstang", 9));
-            VareOversigt.Add(new Vare("Astronaut", 24));
-            VareOversigt.Add(new Vare("Kung Fu", 10));
-
-            LbxIceType.ItemsSource = VareOversigt;
+            LbxInfo.Items.Clear();
+            LbxInfo.Items.Add("Dette er en Magnum");
+            LbxKøbsInfo.Items.Add("1x Magnum");
         }
 
-        #endregion
-
-        private void BtnAddIce_Click(object sender, RoutedEventArgs e)
+        private void BtnAstronaunt_Click(object sender, RoutedEventArgs e)
         {
-            vare.Add(VareOversigt[].Navn);
-            DtgIceInfo.ItemsSource = vare;
-            DtgIceInfo.Items.Refresh();
-
-
-            AntalVareTilbage.Add(new Vare(LbxIceType.SelectedValue.ToString(), antalIs));
-            DtgIcePrice.ItemsSource = AntalVareTilbage;
-            DtgIcePrice.Items.Refresh();
-
-            antalIs -= 1;
+            LbxInfo.Items.Clear();
+            LbxInfo.Items.Add("Dette er en Astronaut");
+            LbxKøbsInfo.Items.Add("1x Astronaut");
         }
 
-        private void Pung()
+        private void BtnKøbenhavnerstang_Click(object sender, RoutedEventArgs e)
         {
-            switch (LbxIceType.SelectedValue)
-            {
-                case 0:
-                    LbPrice.Content = VareOversigt[0].pris;
-                    break;
-                case 1:
-                    LbPrice.Content = VareOversigt[1].pris;
-                    break;
-                case 2:
-                    LbPrice.Content = VareOversigt[2].pris;
-                    break;
-                case 3:
-                    LbPrice.Content = VareOversigt[3].pris;
-                    break;
-                case 4:
-                    LbPrice.Content = VareOversigt[4].pris;
-                    break;
-                default:
-                    break;
-            }
+            LbxInfo.Items.Clear();
+            LbxInfo.Items.Add("Dette er en københavner stang");
+            LbxKøbsInfo.Items.Add("1x Københavner stang");
+        }
+
+        private void BtnTwister_Click(object sender, RoutedEventArgs e)
+        {
+            LbxInfo.Items.Clear();
+            LbxInfo.Items.Add("Dette er en Twister");
+            LbxKøbsInfo.Items.Add("1x Twiser");
+        }
+
+        private void BtnFilur_Click(object sender, RoutedEventArgs e)
+        {
+            LbxInfo.Items.Clear();
+            LbxInfo.Items.Add("Dette er en Filur");
+            LbxKøbsInfo.Items.Add("1x Magnum");
+        }
+
+        private void BtnKungFu_Click(object sender, RoutedEventArgs e)
+        {
+            LbxInfo.Items.Clear();
+            LbxInfo.Items.Add("Dette er en Kung Fu");
+            LbxKøbsInfo.Items.Add("1x Kung Fu");
         }
     }
 
