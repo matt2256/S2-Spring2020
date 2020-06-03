411de705144528783038b803ce840d0c6a175c5e
matt2256 <mads@mmm-m.dk>
2020-06-03 09:20:42 +0200

    Adde MatchupEntryModel + properties
    added ID property to MatchupModel

---
+++ TournamentTracker/TrackerLibary/MatchupEntryModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrackerLibary
+{
+    public class MatchupEntryModel
+    {
+        public int ID { get; set; }
+        public TeamModel TeamCompeting { get; set; }
+        public double Score { get; set; }
+        public MatchupModel ParentMatchup { get; set; }
+    }
+}
--- TournamentTracker/TrackerLibary/MatchupModel.cs
+++ TournamentTracker/TrackerLibary/MatchupModel.cs
@@ -6,5 +6,9 @@ namespace TrackerLibary
 {
     public class MatchupModel
     {
+        public int ID { get; set; }
+        public List<MatchupEntryModel> Entries { get; set; } = new List<MatchupEntryModel>();
+        public TeamModel teamModel { get; set; }
+        public int MatchupRound { get; set; }
     }
 }
