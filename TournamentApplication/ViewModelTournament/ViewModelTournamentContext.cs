2dbea4247b12e8f981e819d4731b7226a33128f9
matt2256 <mads@mmm-m.dk>
2020-06-08 10:23:08 +0200

    Added a WPF project to the solution called UI
    Added dbset<> properties to ViewModelTournament
    
    Added a empty construtor to ViewModelTournament
    
    Updated my to do list

---
+++ TournamentApplication/ViewModelTournament/ViewModelTournamentContext.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TrackerLib;
+
+namespace ViewModelTournament
+{
+    class ViewModelTournamentContext : DbContext
+    {
+        /// <summary>
+        /// Represent a empty constructor
+        /// </summary>
+        public ViewModelTournamentContext(){}
+
+
+        #region dbset properties 
+        /// <summary>
+        /// Represent a dbset<> property of PersonModel
+        /// </summary>
+        public DbSet<PersonModel> Person { get; set; }
+
+        /// <summary>
+        /// Represent a dbset<> property of MatchupModel
+        /// </summary>
+        public DbSet<MatchupModel> Matchup { get; set; }
+
+        /// <summary>
+        /// Represent a dbset<> property of MachtupEntryModel
+        /// </summary>
+        public DbSet<MatchupEntryModel> MatchupEntries { get; set; }
+
+        /// <summary>
+        /// Represent a dbset<> property of TeamModel
+        /// </summary>
+        public DbSet<TeamModel> Team { get; set; }
+
+        /// <summary>
+        /// Represent a dbset<> property or TournamentModel
+        /// </summary>
+        public DbSet<TournamentModel> Tournament { get; set; }
+        #endregion
+
+
+    }
+}
